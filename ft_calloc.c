95282471bbce4a39d60c3b4c71882209e00b17d2
Pepijn Oude Vrielink <poude-vr@f0r4s26.codam.nl>
2024-01-21 21:11:56 +0100

    testing

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,50 @@
 /*   By: poude-vr <poude-vr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:34:54 by poude-vr          #+#    #+#             */
-/*   Updated: 2024/01/21 21:03:00 by poude-vr         ###   ########.fr       */
+/*   Updated: 2024/01/21 21:11:40 by poude-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
 
+void	*ft_memset(void *s, int c, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = s;
+	while (i < len)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (s);
+}
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	mem_size;
 	void	*allocated;
 
+	allocated = malloc(1);
 	mem_size = (nmemb * size);
 	if (!mem_size)
-		return (0);
+		return (allocated);
 	allocated = malloc(mem_size);
 	if (allocated != NULL)
 		ft_memset(allocated, 0, mem_size);
 	return (allocated);
+}
+
+int main()
+{
+	void *str = ft_calloc(0,0);
+	if (str == ((void *)0))
+	{	
+		printf("failed");
+	}
+	else
+	printf("succes");
 }
